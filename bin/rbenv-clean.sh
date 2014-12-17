617473f12d002721d4a40c24c6851f155738a2c7
Josh Burns <joshyburnss@gmail.com>
2014-12-17 15:48:04 +1100

    add header info

---
+++ bin/rbenv-clean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+#
+# rbenv-clean.sh(1) - Shell script to clean up rbenv installed gems and versions.
+# Released under the MIT License
+#
+# https://github.com/sableloki/rbenv-clean
+#
