6c10efa89108aa6538fd520565f20294499fcd1c
Josh Burns <joshyburnss@gmail.com>
2014-12-17 15:57:17 +1100

    usage output

--- bin/rbenv-clean.sh
+++ bin/rbenv-clean.sh
@@ -6,3 +6,22 @@
 #
 # https://github.com/sableloki/rbenv-clean
 #
+
+VERSION="0.1.0"
+TEST=1
+
+usage() {
+      cat <<-EOF
+  Usage: rbenv-clean [options] [command]
+
+  Options:
+    -h, --help           output help information
+    -v, --version        display version number
+    -r, --restore        restore installed versions
+
+  Commands:
+    all                  clean everything
+    rubies               clean just rubies
+    gems                 clean just gems
+EOF
+}
