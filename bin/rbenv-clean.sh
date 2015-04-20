050ee76eb49782851c576b50c69707bf7ef04b06
Josh Burns <mail@joshburns.ml>
2015-04-21 01:20:53 +1000

    nope

--- bin/rbenv-clean.sh
+++ bin/rbenv-clean.sh
@@ -14,7 +14,7 @@ set -e
 uninstall() {
     list=`gem list --no-versions`
     for gem in $list; do
-        gem uninstall $gem -aIxf
+        gem uninstall $gem -aIx
     done
     gem list
     gem install bundler
